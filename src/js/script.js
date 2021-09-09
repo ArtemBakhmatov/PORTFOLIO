497e688443c540da36710e47c3d7ae6c30325aa7
Artem <ip.bakhmatov@mail.ru>
2021-09-09 22:21:16 +0300

    Вёрстка первого экрана,Продолжение

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+let hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', function() {
+    menu.classList.add('menu_active');
+});
+
+closeElem.addEventListener('click', function() {
+    menu.classList.remove('menu_active');
+});
