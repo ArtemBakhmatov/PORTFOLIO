b3b63209913f18d36287530044d172f1bc355514
Artem <ip.bakhmatov@mail.ru>
2021-09-17 20:08:07 +0300

    Адаптация первого экрана

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 let hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.menu__close');
+    menuLink = document.querySelectorAll('.menu__link')
 
 hamburger.addEventListener('click', function() {
     menu.classList.add('menu_active');
@@ -8,5 +9,11 @@ hamburger.addEventListener('click', function() {
 
 closeElem.addEventListener('click', function() {
     menu.classList.remove('menu_active');
+});
+
+menuLink.forEach(function(item) {
+    item.addEventListener('click', function() {
+        menu.classList.toggle('menu_active');
+    });
 });
 
